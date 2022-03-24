ecde8ec56eb3e311e689e9ed97396a03bbea5c0a
Jose <joseapontec@gmail.com>
2022-03-24 17:52:55 -0500

    Initial Commit

---
+++ AutoResizeCell/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoResizeCell
+//
+//  Created by Jose Aponte on 24/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
